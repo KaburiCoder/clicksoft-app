637a4c5043e0da94c4d18e1336d62b2066f678e9
Kaburi <specialemail90@gmail.com>
2024-03-18 16:44:40 +0900

    .

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,4 @@
 export const APP_URL =
-  (process.env.NODE_ENV === "production" ? "https://" : "http://") +
-  process.env.NEXT_PUBLIC_DOMAIN;
+  (process.env.NODE_ENV === "production"
+    ? `https://${process.env.NEXT_PUBLIC_DOMAIN}`
+    : "http://localhost:3010");
