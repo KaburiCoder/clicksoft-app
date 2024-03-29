582427bbf9ca1ccc5b279fb3008677a94bef9863
Kaburi <specialemail90@gmail.com>
2024-03-29 19:58:19 +0900

    .

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "클릭소프트 앱",
+    short_name: "클릭소프트 앱",
+    description: "클릭소프트 앱",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
